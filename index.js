6d649427155f65596e8112f15198e7704a2ca2ab
squarfiuz07 <70509576+squarfiuznath@users.noreply.github.com>
2021-05-07 11:03:43 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+By squarfiuz07
+Discanvas
+My Youtube Channel : https://www.youtube.com/channel/UC2ACluqDgpeNbexpdaTACmw
+My Instagram : https://www.instagram.com/squarfiuz07/
+My Discord : https://discord.gg/A59kDPN 
+*/
+
+module.exports.RankCard = require('./canvas/rankcard');
